46dd888fa457757b7caaac70c4c11ea020737c51
DaPoetiQue <afreemedian@gmail.com>
2024-09-03 21:17:25 +0200

    Added initial project files to git repo.

---
+++ Libraries/Debugger/Interface/ILogger.cs
@@ -0,0 +1,18 @@
+﻿namespace Debugger
+{
+    /// <summary>
+    /// Defines methods for logging messages at various verbosity levels and handling exceptions.
+    /// </summary>
+    /// <remarks>
+    /// Implementations of this interface should provide mechanisms to log messages of different severities
+    /// and handle exceptions based on the specified verbosity level and source information.
+    /// </remarks>
+    public interface ILogger
+    {
+        void Log<T>(LogVerbosity verbosity, string message, T source = null) where T: class;
+        void LogInfo<T>(string message, T source = null) where T : class;
+        void LogWarning<T>(string message, T source = null) where T : class;
+        void LogError<T>(string message, T source = null) where T : class;
+        void ThrowException<T>(string exception, T source = null) where T : class;
+    }
+}
---
+++ Libraries/Native/LogMonobehaviour.cs
@@ -0,0 +1,19 @@
+﻿using Debugger;
+using UnityEngine;
+
+namespace Native
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class LogMonoBehaviour: MonoBehaviour, Debugger.ILogger
+    {
+        [field: SerializeField]
+        public Debugger.LogVerbosity EnabledLogVerbosity { get; private set; }
+
+        public void Log()
+        {
+
+        }
+    }
+}
